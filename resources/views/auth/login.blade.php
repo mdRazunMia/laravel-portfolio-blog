842058997c06da0a43f4ea11f2071bfd7529b245
mdRazunMia <razun.neub.cse@gmail.com>
2022-09-07 22:48:47 +0600

    Change some desing again.

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -1,9 +1,9 @@
 @extends('layouts.main')
 
 @section('content')
-<div class="container">
+<div class="container" style="min-height: 800px">
     <div class="row justify-content-center">
-        <div class="col-md-8">
+        <div class="col-md-8 mt-2 mb-2">
             <div class="card">
                 <div class="card-header">{{ __('Login') }}</div>
 
--- resources/views/blog/index.blade.php
+++ resources/views/blog/index.blade.php
@@ -1,34 +1,38 @@
 @extends('layouts.main')
 @section('title',"PortfolioBlog | Blog")
 @section('content')
-    @if (Session::has('success'))
-        <div class="row text-centern mt-2">
-            <div class="col-4"></div>
-            <div  x-data="{show: true }" x-show="show" x-init="setTimeout(()=> show = false, 3000)"  class="col-4 alert alert-success alert-dismissible">
-                    <p class="btn-info">{{ Session::get('success') }}</p>
+    <div class="container" style="min-height: 800px">
+            @if (Session::has('success'))
+            <div class="row text-centern mt-2">
+                <div class="col-4"></div>
+                <div  x-data="{show: true }" x-show="show" x-init="setTimeout(()=> show = false, 3000)"  class="col-4 alert alert-success alert-dismissible">
+                        <p class="btn-info">{{ Session::get('success') }}</p>
+                </div>
+                <div class="col-4"></div>
             </div>
-            <div class="col-4"></div>
+        @endif
+        <div class="row text-center h-auto">
+        @foreach ($blogs as $blog )
+            <div class="col-8 bg-info m-auto rounded mt-2 mb-4">
+                <h2 class="mt-2" style="">{{ $blog->title }}</h2>
+                <p>Created By: {{ $blog->user->name }} | Posted At: {{ $blog->created_at->diffForHumans() }}</p>
+                <hr>
+                <p>{!! $blog->body !!}</p>
+                <hr>
+            @if (auth::user())
+                @if(auth::user()->user_type == 'admin')
+                        <div class="float-left">
+                            <a class="btn btn-primary mb-2 center-block" href="{{ route('blog.edit',$blog->id) }}">Edit</a>
+                            <form method="POST" action="{{ route('blog.destroy',$blog->id) }}">
+                                @csrf
+                                @method('DELETE')
+                                <input class="btn btn-danger center-block mb-1" type="submit"  value="Delete"/>
+                            </form>
+                        </div>
+                    @endif
+            @endif
+            </div>
+        @endforeach
         </div>
-    @endif
-    <div class="row text-center h-auto">
-       @foreach ($blogs as $blog )
-        <div class="col-8 bg-info m-auto rounded mt-2 mb-4">
-            <h2 class="mt-2" style="">{{ $blog->title }}</h2>
-            <p>Created By: {{ $blog->user->name }} | Posted At: {{ $blog->created_at->diffForHumans() }}</p>
-            <hr>
-            <p>{!! $blog->body !!}</p>
-            <hr>
-           @if(auth::user()->user_type == 'admin')
-               <div class="float-left">
-                 <a class="btn btn-primary mb-2 center-block" href="{{ route('blog.edit',$blog->id) }}">Edit</a>
-                <form method="POST" action="{{ route('blog.destroy',$blog->id) }}">
-                    @csrf
-                    @method('DELETE')
-                    <input class="btn btn-danger center-block mb-1" type="submit"  value="Delete"/>
-                </form>
-           </div>
-           @endif
-        </div>
-       @endforeach
     </div>
 @endsection
